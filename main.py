800bc741a15d1623f0362e3814bb947d100a7dd7
Tiupiu2007 <chiesa.nicola12@gmail.com>
2026-08-31 19:53:43 +0200

    Remove unused program entry point

--- main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from gui import IsekaiWindow
-from PyQt6.QtWidgets import QApplication
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = IsekaiWindow()
-    window.show()
-    sys.exit(app.exec())
